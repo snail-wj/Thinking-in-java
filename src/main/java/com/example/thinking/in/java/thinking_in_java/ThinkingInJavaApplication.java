58e947989fa863c21a61a5bd8b6b450f132a1d0c
wangjing06 <wangjing06@kuaishou.com>
2019-02-25 23:57:40 +0800

    thinking in java

---
+++ src/main/java/com/example/thinking/in/java/thinking_in_java/ThinkingInJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.thinking.in.java.thinking_in_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkingInJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkingInJavaApplication.class, args);
+	}
+
+}
